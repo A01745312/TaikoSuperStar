d12cfc321fb3130ffecf3dee343205a0e4c5da02
A01745312 <A01745312@itesm.mx>
2022-04-23 17:54:53 -0500

    Consultas a BD

--- index.js
+++ index.js
@@ -2,16 +2,31 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 
 // Crear servidor
 const app = express();
 
+
+// Conectar BD
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'telcel'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+});
+
 // Middleware
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
 
 
 app.engine('html',require('ejs').renderFile);
@@ -21,6 +36,11 @@ app.set('view engine', 'ejs');
 app.get('/main', (req,res) => {
     res.sendFile(path.join(__dirname,'views','index.html'));
 });
+
+// ----------------------------------------------------------------------
+
+    //   ******** PLANTILLAS PARA CONEXIÓN BD ***********
+
 
 app.get('/plantillaEJS', (req,res)=> {
     // Simular una consulta a la BS
@@ -34,10 +54,44 @@ app.get('/plantillaEJS', (req,res)=> {
 
     ];
     res.render('ejemploEJS.html',{
-        personas:consulta,
+        personas: consulta,
         sesion: "Autorizada",
         fecha: 2021
     });
 });
 
-app.listen(8081,()=>console.log("Servidor en línea en el puerto 8081"));
+
+// Consulta a BD MySQL
+
+    // Por tabla
+app.get('/destino',(req,res)=>{
+    const consulta = 'SELECT * FROM destino';
+    connection.query(consulta,(error,results) => {
+      if (error) throw error;
+      if (results.length > 0){
+          res.json(results);
+      }else{
+          res.send('No hay resultados');
+      }
+    });
+});
+
+
+    //Por ID
+app.get('/destino/:id',(req,res)=>{
+    const {id} = req.params;
+    const consulta = `SELECT * FROM destino WHERE clave = ${id}`;
+    connection.query(consulta,(error,results) => {
+        if (error) throw error;
+        if (results.length > 0){
+            res.json(results);
+        }else{
+            res.send('No hay resultados');
+        }
+    });
+});
+
+// -----------------------------------------------------------------------
+
+
+app.listen(8081,()=> console.log("Servidor en línea en el puerto 8081"));
