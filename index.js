db4f56766d81590961f2d43b32dbb446f662e7c1
A01745312 <A01745312@itesm.mx>
2022-04-23 23:36:33 -0500

    conexión BD

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const mysql = require('mysql');
 const app = express();
 
 
-// Conectar BD
+//Conectar BD
 
-/* const connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'telcel'
+    database: 'taiko_super_star'
 });
 
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to database");
-}); */
+});
 
 // Middleware
 app.use(express.static(path.join(__dirname,'public')));
@@ -37,12 +37,34 @@ app.get('/main', (req,res) => {
     res.sendFile(path.join(__dirname,'views','index.html'));
 });
 
+
+app.get('/login',(req,res)=>{
+    const consulta = 'SELECT * FROM login';
+    connection.query(consulta,(error,results) => {
+      if (error) throw error;
+      if (results.length > 0){
+        res.sendFile(path.join(__dirname,'views','login.html'));
+      }else{
+          res.send('No hay resultados');
+      }
+    });
+});
+
+app.post('/pruebaDatos',(req,res)=>{
+    console.log('Pase por prueba datos');
+    console.log(req.body);
+    res.redirect('/main');
+});
+
+app.listen(8081,()=> console.log("Servidor en línea en el puerto 8081"));
+
+
 // ----------------------------------------------------------------------
 
     //   ******** PLANTILLAS PARA CONEXIÓN BD ***********
 
 
-app.get('/plantillaEJS', (req,res)=> {
+/* app.get('/plantillaEJS', (req,res)=> {
     // Simular una consulta a la BS
     const consulta = [
         {usuarioUsuario: 'Maria', password: "efewf", edad:18},
@@ -58,40 +80,37 @@ app.get('/plantillaEJS', (req,res)=> {
         sesion: "Autorizada",
         fecha: 2021
     });
-});
+}); */
 
 
 // Consulta a BD MySQL
 
     // Por tabla
-app.get('/destino',(req,res)=>{
-    const consulta = 'SELECT * FROM destino';
-    connection.query(consulta,(error,results) => {
-      if (error) throw error;
-      if (results.length > 0){
-          res.json(results);
-      }else{
-          res.send('No hay resultados');
-      }
+    app.get('/destino',(req,res)=>{
+        const consulta = 'SELECT * FROM destino';
+        connection.query(consulta,(error,results) => {
+          if (error) throw error;
+          if (results.length > 0){
+              res.json(results);
+          }else{
+              res.send('No hay resultados');
+          }
+        });
     });
-});
-
-
-    //Por ID
-app.get('/destino/:id',(req,res)=>{
-    const {id} = req.params;
-    const consulta = `SELECT * FROM destino WHERE clave = ${id}`;
-    connection.query(consulta,(error,results) => {
-        if (error) throw error;
-        if (results.length > 0){
-            res.json(results);
-        }else{
-            res.send('No hay resultados');
-        }
+    
+    
+        //Por ID
+    app.get('/usuario/:id',(req,res)=>{
+        const {id} = req.params;
+        const consulta = `SELECT * FROM login WHERE clave = ${id}`;
+        connection.query(consulta,(error,results) => {
+            if (error) throw error;
+            if (results.length > 0){
+                res.json(results);
+            }else{
+                res.send('No hay resultados');
+            }
+        });
     });
-});
-
-// -----------------------------------------------------------------------
-
-
-app.listen(8081,()=> console.log("Servidor en línea en el puerto 8081"));
+    
+    // -----------------------------------------------------------------------
